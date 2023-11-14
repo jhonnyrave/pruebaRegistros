4aa38dea10b7b4715a83d2f8091241e8ced69e44
jhonnyrave <jhonnyrave09@hotmail.com>
2023-11-13 19:07:32 -0500

    instalacion y configuracion de sweealert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js','public/libreria/toastr/toastr.js','public/libreria/toastr/toastr.scss'])
 </head>
